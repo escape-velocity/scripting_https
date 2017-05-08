2ae51b50f8735a8c86c46b9e6f9e0e5096146f2f
Bill <networker747@gmail.com>
2017-05-08 21:38:22 +0000

    added in a callback for printHTML

---
+++ HTMLChunks3.js
@@ -0,0 +1,32 @@
+
+
+var https = require('https');
+
+function getHTML (options, callback)  {
+  var chunks = '';
+  //defined the empty variable as a string
+
+
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      chunks += data;
+      //inside of the data function we added the append to the variable
+    });
+    response.on('end', function() {
+      callback(chunks);
+      //output the chunks to the terminal
+    });
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+//call the function to run it
+getHTML(requestOptions, printHTML);
