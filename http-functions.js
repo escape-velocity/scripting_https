314f5f4012eb9212e32a2021b12dcf1c31031f3c
Bill <networker747@gmail.com>
2017-05-09 00:18:22 +0000

    put the function into a module and called from another file

---
+++ http-functions.js
@@ -0,0 +1,33 @@
+
+
+var https = require('https');
+
+
+module.exports = function getHTML(options, callback)  {
+  var chunks = '';
+  //defined the empty variable as a string
+
+
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      chunks += data;
+      //inside of the data function we added the append to the variable
+    });
+    response.on('end', function() {
+      callback(chunks);
+      //output the chunks to the terminal
+    });
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+//call the function to run it
+module.exports(requestOptions, printHTML);
