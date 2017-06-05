2e8efb8f43455112ed48c2e29043613581297d1c
Bill <networker747@gmail.com>
2017-06-05 21:05:20 +0000

    calling exported module

--- callbacks/print_lowercase.js
+++ callbacks/print_lowercase.js
@@ -4,11 +4,8 @@ var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step6/lowercase.html'
 };
-
 function printLowerCase (html) {
   console.log(html.toLowerCase());
-
-
 }
 
 getHTML(requestOptions, printLowerCase);
